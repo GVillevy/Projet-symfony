d319d4df52a22ffa9b092e796820b4daa1379ef7
Geoffrey <villevygeoffrey@gmail.com>
2021-10-09 15:27:04 +0200

    Ajout des articles

--- src/Controller/GVNewsController.php
+++ src/Controller/GVNewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,17 @@ class GVNewsController extends AbstractController
     #[Route('/news', name: 'news')]
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+
         return $this->render('gv_news/index.html.twig', [
             'controller_name' => 'GVNewsController',
+            'articles' => $articles
         ]);
+    }
+
+    #[Route('/news/12', name: 'news_show')]
+    public function show(){
+        return $this->render('gv_news/show.html.twig');
     }
 }
