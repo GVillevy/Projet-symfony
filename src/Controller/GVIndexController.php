8e366784eeba73921509c5375b76dedaa1a057ac
Geoffrey <villevygeoffrey@gmail.com>
2021-10-12 18:38:57 +0200

    Ajout du PlayOfTheWeek avec crud

--- src/Controller/GVIndexController.php
+++ src/Controller/GVIndexController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\News;
+use App\Entity\PlayOfTheWeek;
 use App\Entity\User;
+use App\Repository\PlayOfTheWeekRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -38,11 +40,28 @@ class GVIndexController extends AbstractController
     }
 
     #[Route('/news/{id}', name: 'news_show', methods: ['GET'])]
-    public function show(News $news): Response
+    public function news_show(News $news): Response
     {
         return $this->render('gv_index/news_show.html.twig', [
             'news' => $news,
         ]);
     }
+
+    #[Route('/potw', name: 'potw_index', methods: ['GET'])]
+    public function potw(PlayOfTheWeekRepository $playOfTheWeekRepository): Response
+    {
+        return $this->render('gv_index/potw.html.twig', [
+            'play_of_the_weeks' => $playOfTheWeekRepository->findAll(),
+        ]);
+    }
+    #[Route('/potw/{id}', name: 'potw_show', methods: ['GET'])]
+    public function potw_show(PlayOfTheWeek $playOfTheWeek): Response
+    {
+        return $this->render('gv_index/potw_show.html.twig', [
+            'play_of_the_week' => $playOfTheWeek,
+        ]);
+    }
+
+
 
 }
